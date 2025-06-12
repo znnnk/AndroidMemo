a1e9e4c77f9bd14c06da64a339243d1eb23f305f
xyrbz <2102417036@qq.com>
2025-06-12 10:03:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidmemo/StartNotification.java
@@ -0,0 +1,33 @@
+package com.example.androidmemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class StartNotification {
+
+    public static void startTimeService(Long time,String title,String text,Context context){
+        int notificationID;
+        SQLiteDatabase dbRead=(new MyDBOpenHelper(context)).getReadableDatabase();
+        Intent intent=new Intent(context,TimeService.class);
+        Cursor result=dbRead.query("tb_Remind",new String[]{"notificationID"},null,null,null,null,null,null);
+        if (result.moveToFirst()){
+            notificationID=result.getInt(0);
+            SQLiteDatabase dbWriter=(new MyDBOpenHelper(context)).getWritableDatabase();
+            ContentValues cv = new ContentValues();
+            cv.put("notificationID", notificationID+1);
+            dbWriter.update("tb_Remind", cv,null, null);
+            dbWriter.close();
+        }else {
+            notificationID=0;
+        }
+        dbRead.close();
+        intent.putExtra("time", time);
+        intent.putExtra("title",title);
+        intent.putExtra("text",text);
+        intent.putExtra("notificationID",notificationID);
+        context.startService(intent);
+    }
+}
